7a8378c04c8c0264eae2a1b095cc40790b998917
Josif <josifvladimirov@protonmail.com>
2025-02-16 17:09:55 +0100

    home page added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,10 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
-    title: "Promptopia",
+    title: "PromptNL",
     description: "Discover and Share AI Prompts"
 }
 
@@ -9,12 +12,15 @@ const RootLayout = ( {children} ) => {
   return (
     <html lang='en'>
         <body>
+          <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
             <main className='app'>
+              <Nav/>
                 {children}
                 </main>
+                </Provider>
         </body>
     </html>
   )
