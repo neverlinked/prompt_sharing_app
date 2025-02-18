14430cc3d3da849060bda2a71032de28d9735b64
Josif <josifvladimirov@protonmail.com>
2025-02-19 00:11:24 +0100

    auth fixed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,53 +1,41 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
-console.log({
-    clientId: process.env.GOOGLE_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      await connectToDB();
+      const user = await User.findOne({ email: session.user.email });
+      session.user.id = user._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        const userExists = await User.findOne({ email: profile.email });
+        
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          });
+        }
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
 });
 
-const handler = NextAuth({
-    providers: [
-        GoogleProvider ({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
-    async session ({session}) {
-        const sesssionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString();
-
-        return session;
-    },
-    async SignIn({profile}) {
-        try {
-            await connectToDB();
-            //CHECK IF A USER ALREADY EXISTS
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-            //IF NOT CREATE A NEW USER AND SAVE IT TO DB
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                });
-            }
-
-            return true;
-          }  catch (error) {
-            console.log(error);
-            return false;
-            }
-        }
-    
-})
-
-export {handler as Get, handler as POST} ;
+export { handler as GET, handler as POST }; // <-- This is crucial!
